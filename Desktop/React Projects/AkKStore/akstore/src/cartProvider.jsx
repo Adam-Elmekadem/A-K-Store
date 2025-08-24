f8dc1a791cb6ba78f93c8113953594208a58d08d
Adam Elmekadem <2002061400454@ofppt-edu.ma>
2025-08-24 22:19:19 +0200

    Delete Desktop/React Projects/AkKStore/akstore directory
    files mistakes

--- Desktop/React Projects/AkKStore/akstore/src/cartProvider.jsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { CartProvider } from "./context/cartContext";
-
-export default function ClientCartProvider({ children }) {
-  return <CartProvider>{children}</CartProvider>;
-}
